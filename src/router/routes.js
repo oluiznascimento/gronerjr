16cf76ee77ffbbc7dd7891baa72d88dadd70e6da
luiz <luiz.compscience@gmail.com>
2025-04-04 09:20:41 -0300

    mensagem do commit

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,12 +3,9 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
+      { path: '', component: () => import('pages/HomePage.vue') }
     ]
   },
-
-  // Always leave this as last one,
-  // but you can also remove it
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue')
